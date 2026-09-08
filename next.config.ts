6cce903a68cbcae57a63bc939c80782de08fc593
Adamarys Muniz <ADAMARYS.MUNIZ-SANCHEZ92@myhunter.cuny.edu>
2026-09-08 16:21:25 -0400

    Removed unnecessary entries

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-  allowedDevOrigins: ["192.168.1.21", "192.168.1.171", "192.168.*"],
-
   images: {
     remotePatterns: [
       {
